9938d61b33d2210c15f6f148afaf2d06796fe199
mdshahjalal5 <muhommodshahjalal@gmail.com>
2023-03-13 15:20:11 +0600

    not serious

---
+++ n.md
@@ -0,0 +1,15 @@
+st=> 3/12/2023, 4:46:56 PM  
+com=> 
+
+> mongoose enum 
+![](2023-03-13-10-03-03.png)
+
+=> mi me => middlewares instance method => pre po => this func => before saving =>  post after saving  => 
+we can insert method => 
+
+> ### mongoose query => same as mongodb query little change 
+![](2023-03-13-14-32-16.png)
+
+> mongoose projection 
+![](2023-03-13-14-34-49.png)
+User snippets vvi 
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const DBConnect = require("./utils/dbConnect");
+// const DBConnect = require("./utils/dbConnect");
 
 const app = require("./app");
 
 // database connection
-DBConnect();
+// DBConnect();
+
+main().catch(err => console.log(err));
+
+async function main() {
+  const goosRes = await mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(goosRes, 'gooes');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 // server
 const port = process.env.PORT || 8080;
